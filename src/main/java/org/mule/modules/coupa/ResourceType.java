5ed73c5698ca31a77454e5cd1bd9b243696d6c20
Franco Bulgarelli <franco@zaubersoftware.com>
2012-01-12 17:49:34 -0300

    License Update
    Now it refers also to coupa4j project

--- src/main/java/org/mule/modules/coupa/ResourceType.java
+++ src/main/java/org/mule/modules/coupa/ResourceType.java
@@ -6,6 +6,10 @@
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
+ *
+ * Coupa Connector com.coupa package contains portions of code based on Coupa4j
+ * http://code.google.com/p/coupa4j/, under a MIT license:
+ * http://www.opensource.org/licenses/mit-license.php.
  */
 
 package org.mule.modules.coupa;
